411841c245cf4e8cd7584da810adcfb42468333b
Angi <mind.edifice@gmail.com>
2024-04-28 14:42:03 +0200

    Completed born into weakness article.

--- src/app/blog/born-into-weakness/page.tsx
+++ src/app/blog/born-into-weakness/page.tsx
@@ -10,21 +10,108 @@ export default function BornIntoWeakness() {
         alt="Bird escaping a cage"
       />
 
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         <p>
-          It is a tough exercise of imagination to escape their invisible
-          chains. Without realizing it, you move in circles just the way they
-          dictate. And how could you do anything else?
+          It’s difficult to escape their invisible chains. We move in circles
+          and how could we do anything else but that? We were trained to be
+          weak, born surrounded by four walls and away from the earth and the
+          animals that gave our ancestors meaning and life. They put us in boxes
+          and progressively introduced more and more ways of control. We are
+          only children when five hours of our day are forcefully taken away
+          without us or our parents having any choice about it. Soon those five
+          hours turn into eight to train us for what will come.
         </p>
 
         <p>
-          You were born into weakness, between 4 concrete walls, away from the
-          earth and the animals that your ancestors came into contact every day
-          as their source of life. They put you into a box, took all your power
-          and slowly introduced more and more ways of control. You were a child
-          when 5 hours of your day were forcefully taken without you or your
-          parents having any choice about it, soon those hours turned 8, to get
-          you used to what will come.
+          When we do not pick our food from the rich ground that replenishes
+          itself, but through a piece of paper, then we are their slaves. We
+          have to dance the way they sing and we learn than in order to survive
+          we need the paper, and to make the paper we need a profession. After a
+          few years of assiduous work we can afford to buy a house, for the
+          lucky ones! But during all that time our personal dreams and our
+          creative selves were tossed aside, but few ever notice it because this
+          is the state in which we are born so it feels as normal as breathing.
+        </p>
+
+        <p>
+          Someone telling us what to do is every day normality and we are not
+          even aware of it. We think we are free, but the whole educational
+          system is about doing what they tell us, and later so is our job and
+          this is the biggest chunk of our waking hours. We almost forget we’re
+          individuals endowed with full capacity to live as we envision it. They
+          don’t allow us to. They created a system in which we need to move as
+          they say or we die. We don’t even realize that at the back of our mind
+          is the fear of having no roof above the head, nothing to eat, homeless
+          and isolated. We don’t realize how abnormal and unnatural this image
+          is, coming from the human animal which evolved to live and strive in
+          groups that work together and support one another.
+        </p>
+
+        <p>
+          The more one thinks about it, the scarier it gets. Is it a coincidence
+          that the structure of society today is that of families as the largest
+          groups? And even those become smaller and more unreliable. All too
+          often individuals die alone, found in their apartment decomposing or
+          half eaten by a starving pet. So let’s just put old people in asylums
+          without realizing that this problem did not exist for the most of this
+          humanity’s existence.
+        </p>
+
+        <p>
+          But what can we do, you may ask. What are the options? How can we
+          shake off their conditioning and the limitations that now build up the
+          whole of society? The answer is inside of you. If you drop all
+          pretense, if you become fully honest with yourself and willing to face
+          your fears, instinct will slowly tell you what is right for you and
+          what you may do to exit the system and start thriving rather than just
+          existing.
+        </p>
+
+        <p>
+          It is difficult to drop the chains but it doesn’t need to be something
+          we achieve over the night. It may take years of slow but decisive
+          work, particularly inner work. Because the conscious mind alone is an
+          easy victim to all these external influences. A good example are
+          inspiring individuals that speak up about many of these problems yet
+          they themselves fall prey to the system rather than move in their own
+          unique direction. These people that become very successful speaking
+          about the liberation of the human soul on YouTube, playing the
+          algorithm using click-bait titles, offering expensive trainings or
+          putting an add every 10 minutes of their video presentations, these
+          people are still actors in the grand play that enslaves us all. The
+          only difference is that they hold a privileged role. You can feel in
+          their drive for money, success and influence that somewhere on the
+          road the pure authentic intention was lost.
+        </p>
+
+        <p>
+          If you audition for those roles, you do nothing else but play their
+          game. But what can we do to destroy it? It’s one of the hardest tasks
+          that there are, to figure it out. It takes a lot of courage to
+          confront all the fear they unconsciously instilled in us and the heavy
+          work of going deep within to find out what our soul actually wants
+          from us. I believe each of us is a beautiful and unique individual
+          that has something to offer to the rest of the world, it doesn’t
+          matter how big or small. And we can bring this back to the world. We
+          can support one another and we don’t need anyone to tell us how and
+          when we can do it.
+        </p>
+
+        <p>
+          We need to break our chains and to exit this virtual reality we find
+          ourselves in. We can see many signs of increasing dystopia, such as
+          their plan to remove cash money and make it all digital. This way it
+          becomes even easier to dictate how you should move when our bank
+          accounts can be frozen anytime they wish. And I do not need to tell
+          you how this world is going down, I am sure you think about it very
+          often yourself. So let us fight to release ourselves and find back our
+          humanity.
         </p>
       </div>
     </>
