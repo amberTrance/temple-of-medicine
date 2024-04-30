4e8a8eb51dd604754911ce6fd4e87ef91fb68d21
Angi <mind.edifice@gmail.com>
2024-04-30 14:45:54 +0200

    Fixed favicon.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Head from "next/head";
 import Navbar from "@/app/layout/navbar/navbar";
 import "./globals.css";
 import { Providers } from "@/app/components/providers";
+import { prefix } from "../../constants";
 
 const montserrat = Montserrat({
   weight: "400",
@@ -17,9 +18,9 @@ export const metadata: Metadata = {
   description:
     "A blog page written with the purpose of sharing insight into one's own mind through the lens of depth psychology. The content varies from dream analysis, philosophical thoughts on societal problems and much more. You can also get in contact with me for dream analysis.",
   icons: {
-    icon: "/leaf.png"
-  }
-  };
+    icon: `${prefix}/leaf.png`,
+  },
+};
 
 export default function RootLayout({
   children,
