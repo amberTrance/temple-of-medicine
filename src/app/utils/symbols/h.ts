8952e9b36674fc574f4d668d470440255bfd7183
Angie <mind.edifice@gmail.com>
2026-08-04 15:52:31 +0200

    Add quote for house symbol.

--- src/app/utils/symbols/h.ts
+++ src/app/utils/symbols/h.ts
@@ -30,7 +30,8 @@ const h: Symbol[] = [
       {
         text: "“Horse” is an archetype that is widely current in mythology and folklore. As an animal it represents the non-human psyche, the subhuman, animal side, the unconscious. That is why horses in folklore sometimes see visions, hear voices, and speak. As a beast of burden it is closely related to the mother-archetype (witness the Valkyries that bear the dead hero to Valhalla, the Trojan horse, etc.). As an animal lower than man it represents the lower part of the body and the animal impulses that rise from there. The horse is dynamic and vehicular power: it carries one away like a surge of instinct. It is subject to panics like all instinctive creatures who lack higher consciousness. Also it has to do with sorcery and magical spells–especially the black night-horses which herald death. It is evident, then, that “horse” is an equivalent of “mother” with a slight shift of meaning. The mother stands for life at its origin, the horse for the merely animal life of the body. If we apply this meaning to the text of our dream, its interpretation will be: The animal life is destroying itself.(dream of a horse killing itself)",
         author: "Carl Jung",
-        source: "The Practice of Psychotherapy, Essays on the Psychology of Transference... p.224",
+        source:
+          "The Practice of Psychotherapy, Essays on the Psychology of Transference... p.224",
       },
     ],
   },
@@ -42,6 +43,11 @@ const h: Symbol[] = [
         text: "Speaking in spatial terms, if we are objective we have to admit that there is a field of the unconscious both above and below us. This same duality applies to the symbolism of the house. The cellar often represents the unconscious in some form, the area of the drives, the instincts; there are innumerable dreams in which coal is in the cellar and there is a fire, or awful animals are in the cellar or burglars have broken in. But exactly the same things happen in the attic. For instance, a crazy person, overwhelmed by the unconscious, has “bats in the belfry” or “mice in the attic.” Ghosts usually rattle their chains in the attic and walk about over our heads. So up in the attic, where it is dark and full of cobwebs and we are a bit crazy, there is just as much a realm of unconsciousness as in the cellar. People frequently dream of thieves getting in from the roof or of demons sitting up there and taking off the tiles, and so on.",
         author: "Marie-Louise von Franz",
         source: "The Problem of the Puer Aeternus, p.142",
+      },
+      {
+        text: "The house, of course, was a symbol of my personality and its conscious field of interests; and the unknown annex represented the anticipation of a new field of interest and research of which my conscious mind was at that time unaware.",
+        author: "Carl Jung",
+        source: "Man and his Symbols, p.54",
       },
     ],
   },
